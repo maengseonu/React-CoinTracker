5e5464769ff1f66d86158d12cc17b2f6d3f1b4f9
maengseonu <aodtjsdn123@gmail.com>
2021-11-18 19:34:11 +0900

    TypeScript-Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.div`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log In</button>
-      </form>
-    </div>
+    <Container>
+      <H1>프론트 엔드</H1>
+    </Container>
   );
 }
 
