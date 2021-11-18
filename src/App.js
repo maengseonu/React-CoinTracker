3a0ad28825dbe73fada936c5d10df9814d290d39
maengseonu <aodtjsdn123@gmail.com>
2021-11-18 15:28:52 +0900

    Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import styled from "styled-components";
+import styled, { keyframes, Keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    border-radius:100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1.5s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>👄</Emoji>
+    </Wrapper>
   );
 }
 
